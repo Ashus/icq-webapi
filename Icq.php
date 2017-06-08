10ecead6300d63ba090f47fe1418458058b1d319
Ashus <github.com@ashus.net>
2017-06-08 20:22:33 +0200

    - added new method getUserInfo - added new method isUserOnline
    Signed-off-by: Ashus <github.com@ashus.net>

--- Icq.php
+++ Icq.php
@@ -5,8 +5,9 @@ namespace carono\components;
 
 class Icq extends IcqCore
 {
-    const URL_SEND = 'https://api.icq.net/im/sendIM';
     const URL_CONNECT = 'https://icq.com/siteim/icqbar/php/proxy_jsonp_connect.php';
+    const URL_SEND = 'https://api.icq.net/im/sendIM';
+    const URL_MEMBERDIR_GET = 'https://api.icq.net/memberDir/get';
 
     public function login($login, $password)
     {
@@ -36,4 +37,39 @@ class Icq extends IcqCore
         ];
         $this->request('GET', self::URL_SEND, $data);
     }
+
+	/**
+	 * @param string $uin
+	 * @return object
+	 */
+	public function getUserInfo($uin)
+    {
+        $data = [
+            "aimsid"  => $this->aimsid,
+            "r"       => '',
+            "f"       => $this->f,
+            "locale" => 'en-us',
+            "infoLevel" => 'full',
+            "t"       => $uin
+        ];
+        $response = $this->request('GET', self::URL_MEMBERDIR_GET, $data);
+        $result = $response->getBody()->getContents();
+        $result = @json_decode($result);
+        return $result;
+    }
+
+	/**
+	 * @param string $uin
+	 * @return bool|null
+	 */
+	public function isUserOnline($uin)
+	{
+		$info = $this->getUserInfo($uin);
+		if (!isset($info->response->data->infoArray[0]->profile))
+			return null;
+
+		$profile = $info->response->data->infoArray[0]->profile;
+		$online = isset($profile->online) ? ($profile->online != 'false') : true;
+		return $online;
+	}
 }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Crude component, development has only just begun
     $icq = new Icq();
     $icq->login($uin, $password);
     $icq->send($uinTo, 'message');
+   	$userInfo = $icq->getUserInfo($uin);
+    $isUserOnline = $icq->isUserOnline($uin);
